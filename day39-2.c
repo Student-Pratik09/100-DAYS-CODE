5d5672e80044e80663b65644c6508a367b55b6bb
Student-Pratik09 <pratikgiri09092006@gmail.com>
2025-09-28 19:35:32 +0530

    my code

---
+++ day39-2.c
@@ -0,0 +1,25 @@
+//Q78: Find the sum of main diagonal elements for a square matrix.
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the size of square matrix: ");
+    scanf("%d", &n);
+
+    int a[n][n];
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i][i];   // main diagonal element
+    }
+
+    printf("Sum of main diagonal elements = %d\n", sum);
+
+    return 0;
+}
